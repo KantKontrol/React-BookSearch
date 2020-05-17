8a249f9b2464279943329602030287823aee7615
KantKontrol <nick.derissio.tech@gmail.com>
2020-05-17 15:37:33 -0400

    added more functions to API util, to use with our api server

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -4,6 +4,18 @@ import axios from "axios";
 export default {
     searchGoogleBooks: function(searchTerms){
         return axios.get(`https://www.googleapis.com/books/v1/volumes?q=${searchTerms}`);
+    },
+
+    saveBook: function(bookData){
+        return axios.post("/api/books", bookData);
+    },
+
+    getSavedBooks: function(){
+        return axios.get("/api/books");
+    },
+
+    deleteBook: function(id){
+        return axios.get(`/api/books/${id}`);
     }
 }
 
